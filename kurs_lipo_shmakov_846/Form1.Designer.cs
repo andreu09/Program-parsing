c1eb1600f94fa76d8799f70876e1c8a6bc28ee39
Андрей Шмаков <shmakov62rus@mail.ru>
2022-04-25 13:51:49 +0300

    add files

---
+++ kurs_lipo_shmakov_846/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿
+namespace kurs_lipo_shmakov_846
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBoxWarnings = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.Menu = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuClose = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuAbout = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBoxWarnings);
+            this.groupBox1.Location = new System.Drawing.Point(12, 626);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1413, 113);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Результаты анализа";
+            // 
+            // textBoxWarnings
+            // 
+            this.textBoxWarnings.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.textBoxWarnings.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxWarnings.CausesValidation = false;
+            this.textBoxWarnings.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.textBoxWarnings.Location = new System.Drawing.Point(6, 26);
+            this.textBoxWarnings.Multiline = true;
+            this.textBoxWarnings.Name = "textBoxWarnings";
+            this.textBoxWarnings.ReadOnly = true;
+            this.textBoxWarnings.ShortcutsEnabled = false;
+            this.textBoxWarnings.Size = new System.Drawing.Size(1401, 81);
+            this.textBoxWarnings.TabIndex = 0;
+            this.textBoxWarnings.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBox);
+            this.groupBox2.Location = new System.Drawing.Point(18, 31);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(1413, 589);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Исходный текст программы на языке Паскаль";
+            // 
+            // textBox
+            // 
+            this.textBox.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.textBox.Location = new System.Drawing.Point(6, 26);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.ReadOnly = true;
+            this.textBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox.ShortcutsEnabled = false;
+            this.textBox.Size = new System.Drawing.Size(1395, 551);
+            this.textBox.TabIndex = 0;
+            this.textBox.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Menu,
+            this.MenuAbout});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1443, 28);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // Menu
+            // 
+            this.Menu.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MenuOpen,
+            this.MenuClose});
+            this.Menu.Name = "Menu";
+            this.Menu.Size = new System.Drawing.Size(59, 24);
+            this.Menu.Text = "Файл";
+            // 
+            // MenuOpen
+            // 
+            this.MenuOpen.Name = "MenuOpen";
+            this.MenuOpen.Size = new System.Drawing.Size(224, 26);
+            this.MenuOpen.Text = "Открыть";
+            this.MenuOpen.Click += new System.EventHandler(this.MenuOpen_Click);
+            // 
+            // MenuClose
+            // 
+            this.MenuClose.Name = "MenuClose";
+            this.MenuClose.Size = new System.Drawing.Size(224, 26);
+            this.MenuClose.Text = "Выйти";
+            this.MenuClose.Click += new System.EventHandler(this.MenuClose_Click);
+            // 
+            // MenuAbout
+            // 
+            this.MenuAbout.Name = "MenuAbout";
+            this.MenuAbout.Size = new System.Drawing.Size(118, 24);
+            this.MenuAbout.Text = "О программе";
+            this.MenuAbout.Click += new System.EventHandler(this.MenuAbout_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1443, 747);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Курсовой проект ЛИПО Шмаков А.Ю. 846 гр.";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem Menu;
+        private System.Windows.Forms.ToolStripMenuItem MenuAbout;
+        private System.Windows.Forms.ToolStripMenuItem MenuOpen;
+        private System.Windows.Forms.ToolStripMenuItem MenuClose;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBoxWarnings;
+    }
+}
+
---
+++ kurs_lipo_shmakov_846/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+
+namespace kurs_lipo_shmakov_846
+{
+    public partial class Form1 : Form
+    {
+        // Директория в которой находится исполняемая программа
+        readonly string CurrentDirectory = Directory.GetCurrentDirectory();
+        // Расположение считываемого файла
+        string DirectoryReadFile;
+        public Form1() => InitializeComponent();
+
+        // Чтение исходных текстов программ на языке Паскаль
+        public void OpenFile()
+        {
+            using OpenFileDialog openFileDialog = new OpenFileDialog();
+                openFileDialog.InitialDirectory = CurrentDirectory;
+                openFileDialog.Filter = "Исходные тексты программ (*.pas)|*.pas";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                // Расположение считываемого файла
+                DirectoryReadFile = openFileDialog.FileName;
+
+                // Считывание содержимого файла в поток
+                var fileStream = openFileDialog.OpenFile();
+
+                using StreamReader reader = new StreamReader(fileStream);
+                string fileContent = reader.ReadToEnd();
+                textBox.Text = fileContent;
+            }
+
+        }
+
+        // Переопределение стандартной функции поиска слова в строке, для расширения возможности поиска без учета регистра
+        public static bool Contains( string source, string toCheck, StringComparison comp)
+        {
+            return source != null && toCheck != null && source.IndexOf(toCheck, comp) >= 0;
+        }
+
+        // Алгоритм 
+        public void Algorithm()
+        {
+            // Счетчик строки
+            int counter = 1;
+            // Сообщение об ошибке
+            string Warning = "Ошибок не найдено!";
+            // Список всех анализируемых лексем
+            List<string> lexemes = new List<string>() { "repeat", "case", "begin", "record" };
+            // Стек лексем, найденных в тексте программы
+            Stack<string> tokens = new Stack<string>();
+
+            // Считываем текст программы по строкам
+            foreach (string line in System.IO.File.ReadLines(DirectoryReadFile))
+            {
+                // В каждой строке ищем все анализируемые лексемы
+                foreach (string lexeme in lexemes)
+                {
+                    // Если лексема найдена, добавляем ее в стек
+                    if (line.Contains(lexeme, StringComparison.OrdinalIgnoreCase))
+                        tokens.Push(lexeme);
+
+                }
+
+                // Если встретили правую скобку типа until
+                if (line.Contains("until", StringComparison.OrdinalIgnoreCase))
+                {
+                    // Если при найденной правой скобке пустой стек
+                    if (tokens.Count == 0)
+                    {
+                        Warning = "(" + DateTime.Now + ") " + "Отсутствует левая скобка: [Строка] = " + counter;
+                        tokens.Clear();
+                        break;
+                    }
+
+                    // Если левая скобка из стека не совпадает с найденной правой по типу
+                    if (tokens.Pop() != lexemes[0])
+                    {
+                        Warning = "(" + DateTime.Now + ") " +  "Не совпадают типы скобок: [Строка] = " + counter;
+                        tokens.Clear();
+                        break;
+                    }
+                }
+
+                // Если встретили правую скобку типа end
+                if (line.Contains("end", StringComparison.OrdinalIgnoreCase))
+                {
+                    // Если при найденной правой скобке пустой стек
+                    if (tokens.Count == 0)
+                    {
+                        Warning = "(" + DateTime.Now + ") " + "Отсутствует левая скобка: " + "[Строка] = " + counter;
+                        tokens.Clear();
+                        break;
+                    }
+
+                    // Проверяем совпадают ли по типу найденная правая скобка с левыми
+                    if ((tokens.Peek() != lexemes[1]) && (tokens.Peek() != lexemes[2]) && (tokens.Peek() != lexemes[3]))
+                    {
+                        Warning = "(" + DateTime.Now + ") " + "Не совпадают типы скобок: [Строка] = " + counter;
+                        tokens.Clear();
+                        break;
+                    }
+                    tokens.Pop();
+                }
+                counter++;
+            }
+            
+            // Если после прохода алготима стек оказался не пустой
+            if (tokens.Count != 0)
+            {
+                Warning = "(" + DateTime.Now + ") " +  "Отсутствует правая скобка: [Строка] = " + counter;
+
+            }
+
+            textBoxWarnings.Text = Warning + "\r\n" + "Отчет об ошибках сохранен:" + Directory.GetCurrentDirectory() + "/Warnings.txt";
+            SaveWarningFile(Warning);
+        }
+
+        // Сохранение результатов анализа в файл
+        public void SaveWarningFile(string Warning)
+        {
+            using FileStream fstream = new FileStream(Directory.GetCurrentDirectory() + "/Warnings.txt", FileMode.OpenOrCreate);
+                // Преобразуем строку в байты
+                byte[] array = System.Text.Encoding.Default.GetBytes(Warning);
+                // Запись массива байтов в файл
+                fstream.Write(array, 0, array.Length);
+        }
+
+        // Клик меню открыть
+        private void MenuOpen_Click(object sender, EventArgs e)
+        {
+            OpenFile();
+            Algorithm();
+        }
+
+        // Клик меню о программе
+        private void MenuAbout_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Дисциплина: Лингвистическое и программное обеспечение САПР." +
+                "Тема курсового проекта: Синтаксический анализ программ.\n" +
+                "Вариант 7: СИНТАКСИЧЕСКИЙ АНАЛИЗ ПАРНЫХ ЭЛЕМЕНТОВ ПРОГРАММ\n" +
+                "Выполнил: Студент 4 курса, Кафедра САПР ВС, Группа 846, Шмаков А.Ю., Проверил Скворцов С.В.");
+        }
+
+        // Клик меню выйти
+        private void MenuClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
